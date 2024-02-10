b431fc3f4e54b8caecb5cf36643caab472afb88a
Egurnova Elena <120087135+monomarine@users.noreply.github.com>
2024-02-10 18:18:48 +0500

    Добавьте файлы проекта.

---
+++ Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Random random = new Random();
+
+            int summ = 0;
+            int count = 0;
+
+            /* определить сумму и количество 
+               отрицательных элементов одномерного массива   */
+            for (int i = 0; i< array.Length; i++) //цикл проходит по всем элементам массива
+            {
+                array[i] = random.Next(-99, 100); //в ячейку массива записывается рандомное значение от -99 до 99 включительно
+                Console.WriteLine($"array[{i}] = {array[i]}"); //параллельный вывод значения на консоль
+
+                if (array[i] < 0) //подсчет суммы и количества отрицательных значений
+                {
+                    count++;
+                    summ+=array[i];
+                }
+            }
+            Console.WriteLine($"сумма отрицательных элементов: {summ}\n" +
+                $"количество отрицательных элементов: {count}");
+
+            /* на основе предыдущего массива произвести копирование 
+               отрицаительных элементов в один массив, положительных в другой */
+
+            int[] negative = new int[count]; //размер массива для отрицательных значений определяется значением count
+            int[] positive = new int[array.Length-count]; //размер массива для полложительных числел - по остаточному принципу
+
+            Array.Sort(array); //сортировка (по умолчанию по возрастанию)
+
+            Console.WriteLine("отрицательные элементы");
+            for (int i = 0; i< count; i++)
+            {
+                negative[i] = array[i];
+                Console.WriteLine($"negative[{i}] = {negative[i]}");
+            }
+
+            Console.WriteLine("положительные элементы");
+            for (int i = 0; i < positive.Length; i++ )
+            {
+                positive[i] = array[count+i]; //запись положительных элементов осуществляется со сдвигом на count значение
+                Console.WriteLine($"positive[{i}] = {positive[i]}");
+            }
+        }
+    }
+}
---
+++ StringArray/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StringArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("объявление строкового массива через список инициализоторов и вывод на консоль черезе foreach\n");
+           
+            string[] textArray = { "text", "test", "sometext" };
+            Array.Sort(textArray);
+            foreach (string item in textArray)
+                Console.WriteLine(item);
+
+
+
+            Console.WriteLine("\n\nобъявление и инициализация массива на 10 строк");
+
+            string[] textArrayGenerated = new string[10];
+            Console.WriteLine("введите символ для генерации строк");
+            char symbol = (char)Console.Read(); //символ на основе которого будут генерироваться строки
+
+            for(int i = 0; i< textArrayGenerated.Length; i++ )
+            {
+                textArrayGenerated[i] = new String(symbol, i);
+                
+                //вывести только те строки, длина которых нечетная
+                if (!(textArrayGenerated[i].Length % 2 == 0))
+                    Console.WriteLine($"i = {i} строка:\t {textArrayGenerated[i]}");
+            }
+
+            
+        }
+    }
+}
